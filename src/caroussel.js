7c145d0cd74dc0fcf52e901d26a2e6f9e9d96d00
fregnani123 <fabianofregnani@hotmail.com>
2024-06-18 11:58:24 -0300

    correções altura contatos

--- src/caroussel.js
+++ src/caroussel.js
@@ -1,22 +1,32 @@
+const itens = document.querySelectorAll(".carousel-item");
+const buttons = document.querySelectorAll("[id^='item-']");
+let currentIndex = 0;
+let interval;
 
-const itens = document.querySelectorAll(".carousel-item")
-const buttons = document.querySelectorAll("[id^='item-']")
 
+function updateCarousel(index) {
+    buttons.forEach(btn => btn.classList.remove('active'));
+    itens.forEach(item => item.classList.remove('active'));
+    buttons[index].classList.add('active');
+    itens[index].classList.add('active');
+}
+
+// Função para iniciar o auto-deslizamento
+// function startAutoSlide() {
+//     interval = setInterval(() => {
+//         currentIndex = (currentIndex + 1) % itens.length;
+//         updateCarousel(currentIndex);
+//     }, 5000); 
+// }
 
 buttons.forEach((btn, index) => {
     btn.addEventListener('click', (e) => {
-        e.preventDefault()
-        
-        buttons.forEach(otherButton => otherButton.style.color = 'white'); // Reset to default color
-
-        itens.forEach((item, itemIndex) => {
-            item.classList.toggle('active', itemIndex === index);
-
-            if (item.classList.contains('active')) {
-                btn.style.color= 'black'; // Change color to blue on activation
-            } 
-        });
-
+        e.preventDefault();
+        clearInterval(interval); // Interrompe o auto-deslizamento
+        updateCarousel(index); // Atualiza o carrossel
+        currentIndex = index; // Atualiza o índice atual
+        startAutoSlide(); // Reinicia o auto-deslizamento
     });
 });
 
+startAutoSlide();
